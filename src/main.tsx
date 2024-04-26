356e55931e14222a9a71793e88faca8821acf256
Christopher <odeniyichristopher7@gmail.com>
2024-04-26 02:51:53 +0100

    chore:landing image fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { RouterProvider } from "react-router-dom";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import App from "./App.tsx";
+
 import "./index.css";
 import router from "./router"
 
-//const router = createBrowserRouter([{ path: "/", element: <div>HEllo</div> }]);
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <App /> */}
+   
 
 <RouterProvider router={router}/>
 
